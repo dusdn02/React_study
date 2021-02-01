a830db46c163ddee63b3477b0d85bf7b4790af57
Lora <lora.lee@companywe.co.kr>
2021-02-01 12:04:01 +0900

    countUp

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react'
 
 class App extends Component{
   state = {
-    hello: 'hello app js!'
+    count:0
   }
 
-  handleChange = () => {
+  countUp = ()=>{
     this.setState({
-      hello: 'bye app js!'
+      count: this.state.count +1
     })
   }
 
   render(){
     return <div className="App">
-      <div>{this.state.hello}</div>
-      <button onClick={this.handleChange}>click Me!</button>
+      <div>{this.state.count}</div>
+      <button onClick={this.countUp}>count up!</button>
     </div>
   }
 }
