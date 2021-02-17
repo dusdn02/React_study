40d44ed033b74250626d0c14fde5ac2a3e3a0d39
dusdn02 <lyw9114@naver.com>
2021-02-17 10:54:17 +0900

    리듀스 고치기

--- src/reduxTest/reducers/index.js
+++ src/reduxTest/reducers/index.js
@@ -1,40 +1,67 @@
-import { combineReducers } from 'redux'
 import * as types from '../actions/ActionTypes'
-import number from './number'
-import color from './color'
-
-import combineReducers from 'redux'
 
 const initialState = {
-    color: 'black',
-    number: 0
+    counters: [
+        {
+            color: 'black',
+            number: 0
+        }
+    ]
 }
 
-// function counter(state = initialState, action) {
-//     switch(action.type){
-//         case types.INCREMENT:
-//             return {
-//                 ...state,
-//                 number: state.number + 1
-//             }
-//         case types.DECREMENT:
-//             return {
-//                 ...state,
-//                 number: state.number - 1
-//             }
-//         case types.SET_COLOR:
-//             return {
-//                 ...state,
-//                 color: action.color
-//             }
-//         default:
-//             return state
-//     }
-// }
+function counter(state = initialState, action) {
+    const { counters } = state
+    switch(action.type){
+        case types.CREATE:
+            return {
+                counters: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number: 0
+                    }
+                ]
+            }
+        case types.REMOVE:
+            return {
+                counters: counters.slice(0, counters.length - 1)
+            }
+        case types.INCREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[applicationCache.index],
+                        number: counters[action.index].number + 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            }
+        case types.DECREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            }
+        case types.SET_COLOR:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            }
+        default:
+            return state
+    }
+}
 
-const reducers = combineReducers({
-    numberData: number,
-    colorData: color
-})
-
-export default reducers
+export default counter
