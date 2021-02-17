59b66f18e81f2e41ad5cab1c6deb92c56c5942fa
dusdn02 <lyw9114@naver.com>
2021-02-17 15:28:39 +0900

    redux-action

--- src/index.js
+++ src/index.js
@@ -82,8 +82,8 @@ import App from './reduxTest/App'
 import './index.css'
 
 import { createStore } from 'redux'
-import reducers from './reduxTest/reducers'
 import { Provider } from 'react-redux'
+import reducers from './reduxTest/modules'
 
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
