f4630f81dc8b5c8a553ef633a60665ce4f82abc5
dusdn02 <lyw9114@naver.com>
2021-02-19 15:51:47 +0900

    미들웨어 예제 세팅

--- src/index.js
+++ src/index.js
@@ -95,23 +95,37 @@
 // )
 
 //주소록
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./ReduxAddressBook/App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./ReduxAddressBook/App";
 
-import { createStore } from "redux";
-import reducers from "./ReduxAddressBook/modules";
-import { Provider } from "react-redux";
-import "./index.css";
+// import { createStore } from "redux";
+// import reducers from "./ReduxAddressBook/modules";
+// import { Provider } from "react-redux";
+// import "./index.css";
 
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+// const store = createStore(
+//   reducers,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
+//redux-starter-kit
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './redux-starter-kit/App';
+import { Provider } from 'react-redux';
+import store from './redux-starter-kit/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App/>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
